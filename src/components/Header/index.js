ef84e415be15683049a110ed0d13cdec7becc664
Bryan <bryanfareznieves@gmail.com>
2024-12-27 21:01:59 -0500

    Update ThemeToggle
    Sirve para cambiar de modo claro y modo oscuro

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom"; // Importar useLocation
 import { menuData } from "./menuData";
+import ThemeToggler from "./ThemeToggler";
 
 const Header = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     const [sticky, setSticky] = useState(false);
     const [openIndex, setOpenIndex] = useState(-1);
-    const [darkMode, setDarkMode] = useState(false);
 
-    const navbarToggleHandler = () => setNavbarOpen(!navbarOpen);
+    const navbarToggleHandler = () => {
+        setNavbarOpen(!navbarOpen);
+    };
 
     const handleStickyNavbar = () => {
-        setSticky(window.scrollY >= 80);
+        if (window.scrollY >= 80) {
+            setSticky(true);
+        } else {
+            setSticky(false);
+        }
     };
 
     const handleSubmenu = (index) => {
-        setOpenIndex(openIndex === index ? -1 : index);
-    };
-
-    const toggleTheme = () => {
-        setDarkMode(!darkMode);
-        document.documentElement.classList.toggle('dark');
+        if (openIndex === index) {
+            setOpenIndex(-1);
+        } else {
+            setOpenIndex(index);
+        }
     };
 
     useEffect(() => {
         window.addEventListener("scroll", handleStickyNavbar);
         return () => window.removeEventListener("scroll", handleStickyNavbar);
     }, []);
+
+    // Usar useLocation para obtener la ruta actual
+    const location = useLocation();
 
     return (
         <header
@@ -57,14 +65,15 @@ const Header = () => {
                         <div>
                             <button
                                 onClick={navbarToggleHandler}
+                                id="navbarToggler"
                                 aria-label="Mobile Menu"
                                 className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
                             >
-                <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                        navbarOpen ? "top-[7px] rotate-45" : ""
-                    }`}
-                />
+                                <span
+                                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
+                                        navbarOpen ? "top-[7px] rotate-45" : ""
+                                    }`}
+                                />
                                 <span
                                     className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
                                         navbarOpen ? "opacity-0" : ""
@@ -77,23 +86,28 @@ const Header = () => {
                                 />
                             </button>
                             <nav
+                                id="navbarCollapse"
                                 className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-gray-200 bg-white px-6 py-4 duration-300 dark:border-gray-700 dark:bg-gray-900 lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
                                     navbarOpen
                                         ? "visibility top-full opacity-100"
                                         : "invisible top-[120%] opacity-0"
                                 }`}
                             >
+                                {/* Menu Items */}
                                 <ul className="block lg:flex lg:space-x-12">
                                     {menuData.map((menuItem, index) => (
                                         <li key={index} className="group relative">
                                             {menuItem.path ? (
-                                                <a
-                                                    href={menuItem.path}
-                                                    className={`flex py-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 
-                            text-gray-900 hover:text-blue-600 dark:text-gray-200 dark:hover:text-white`}
+                                                <Link
+                                                    to={menuItem.path}
+                                                    className={`flex py-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${
+                                                        location.pathname === menuItem.path
+                                                            ? "text-primary dark:text-white"
+                                                            : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
+                                                    }`}
                                                 >
                                                     {menuItem.title}
-                                                </a>
+                                                </Link>
                                             ) : (
                                                 <>
                                                     <p
@@ -102,15 +116,15 @@ const Header = () => {
                                                     >
                                                         {menuItem.title}
                                                         <span className="pl-3">
-                              <svg width="25" height="24" viewBox="0 0 25 24">
-                                <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
-                                    fill="currentColor"
-                                />
-                              </svg>
-                            </span>
+                                                          <svg width="25" height="24" viewBox="0 0 25 24">
+                                                            <path
+                                                                fillRule="evenodd"
+                                                                clipRule="evenodd"
+                                                                d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
+                                                                fill="currentColor"
+                                                            />
+                                                          </svg>
+                                                        </span>
                                                     </p>
                                                     <div
                                                         className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-gray-900 lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
@@ -118,13 +132,13 @@ const Header = () => {
                                                         }`}
                                                     >
                                                         {menuItem.submenu.map((submenuItem, index) => (
-                                                            <a
-                                                                href={submenuItem.path}
+                                                            <Link
+                                                                to={submenuItem.path}
                                                                 key={index}
                                                                 className="block rounded py-2.5 text-sm text-gray-900 hover:text-blue-600 dark:text-gray-200 dark:hover:text-white lg:px-3"
                                                             >
                                                                 {submenuItem.title}
-                                                            </a>
+                                                            </Link>
                                                         ))}
                                                     </div>
                                                 </>
@@ -134,24 +148,23 @@ const Header = () => {
                                 </ul>
                             </nav>
                         </div>
+                        {/* Auth Buttons and Theme Toggle */}
                         <div className="flex items-center justify-end pr-16 lg:pr-0">
-                            <a
-                                href="/signin"
+                            <Link
+                                to="/signin"
                                 className="hidden px-7 py-3 text-base font-medium text-gray-900 hover:opacity-70 dark:text-white md:block"
                             >
                                 Sign In
-                            </a>
-                            <a
-                                href="/signup"
+                            </Link>
+                            <Link
+                                to="/signup"
                                 className="ease-in-up hidden rounded-sm bg-blue-600 px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9"
                             >
                                 Sign Up
-                            </a>
-                            <button
-                                onClick={toggleTheme}
-                                className="flex h-8 w-8 items-center justify-center rounded-md text-gray-900 dark:text-white"
-                            >
-                            </button>
+                            </Link>
+                            <div>
+                                <ThemeToggler />
+                            </div>
                         </div>
                     </div>
                 </div>
